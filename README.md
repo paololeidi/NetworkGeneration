610e1994e5738cfd9f488fe216d58df42e1d6a8c
Paolo Leidi <leidi.paolo98@gmail.com>
2024-07-31 18:06:52 +0200

    Split queries 2 and 3

---
+++ README.md
@@ -0,0 +1,10 @@
+# Network generation
+
+
+### Hard-coded
+Pipeline queries
+Merge queries
+Split window parameters
+
+### Random generation
+Split choice of window queries
--- src/main/java/org/example/networks/Split.java
+++ src/main/java/org/example/networks/Split.java
@@ -6,22 +6,31 @@ import org.example.queries.WindowQuery;
 
 public class Split {
     private Stream inputStream;
-    private WindowQuery query;
+    private WindowQuery query1;
+    private Stream intermediateStream;
+    private WindowQuery query2;
+    private WindowQuery query3;
     private Stream outputStream1;
     private Stream outputStream2;
 
     public Split(Stream inputStream) {
         this.inputStream = inputStream;
-        this.query = RandomQueryFactory.createRandomWindowQuery(inputStream);
-        this.outputStream1 = new Stream(inputStream.getName() + "_" + query.getWindow().toString() + "_1");
-        this.outputStream2 = new Stream(inputStream.getName() + "_" + query.getWindow().toString() + "_2");
+        this.query1 = RandomQueryFactory.createRandomWindowQuery(inputStream);
+        this.intermediateStream = query1.getOutputStream();
+        this.query2 = RandomQueryFactory.createRandomWindowQuery(intermediateStream);
+        this.query3 = RandomQueryFactory.createRandomWindowQuery(intermediateStream);
+        outputStream1 = query2.getOutputStream();
+        outputStream2 = query3.getOutputStream();
     }
 
     @Override
     public String toString() {
         return "Split{" +
                 "inputStream=" + inputStream +
-                ",\n query=" + query +
+                ",\n query1=" + query1 +
+                ",\n intermediateStream=" + intermediateStream +
+                ",\n query2=" + query2 +
+                ",\n query3=" + query3 +
                 ",\n outputStream1=" + outputStream1 +
                 ",\n outputStream2=" + outputStream2 +
                 '}';
